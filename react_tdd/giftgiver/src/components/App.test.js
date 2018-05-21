1d2febb1273e43157b3eaf0b565826b4f23160dd
Shrinidhi <shrinidhisk369@gmail.com>
2018-05-21 15:00:30 +0530

    React Projects

---
+++ react_tdd/giftgiver/src/components/App.test.js
@@ -0,0 +1,15 @@
+// setup file
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const app = shallow(<App />);
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot();
+});
